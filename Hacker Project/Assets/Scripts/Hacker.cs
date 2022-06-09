78186c79ecf44c3a91984fc481a6bf7d74312856
Gold <dzharzard96@gmail.com>
2022-06-09 13:54:50 -0500

    -Added variables

--- Hacker Project/Assets/Scripts/Hacker.cs
+++ Hacker Project/Assets/Scripts/Hacker.cs
@@ -4,16 +4,26 @@ using UnityEngine;
 
 public class Hacker : MonoBehaviour
 {
+    string greeting = "Scanner Finished. Human detected";
     // Start is called before the first frame update
     void Start()
     {
-        Terminal.WriteLine("Hello, you were choosen to perform one of our experiments");
+        
+       ShowMainMenu(greeting);
+        
+    }
+
+    void ShowMainMenu(string welcomeMessage)
+    {
+        
+        Terminal.ClearScreen();
+        Terminal.WriteLine(welcomeMessage);
+       Terminal.WriteLine("Hello, you were choosen to perform one of our experiments");
         Terminal.WriteLine("Please, select your test to prove your ability");
         Terminal.WriteLine("Press 1 for a Local Home");
         Terminal.WriteLine("Press 2 for a Local Shop");
         Terminal.WriteLine("Press 3 for CIA Headquarters");
         Terminal.WriteLine("Enter your selection: ");
-        
     }
 
     // Update is called once per frame
